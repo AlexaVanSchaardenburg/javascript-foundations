7a9c3792e5368f1fa1ac766c1810367572c3e214
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-03-29 17:02:35 -0600

    Complete the hobbit mythical creatures file and tests

--- mythical-creatures/exercises/hobbit.js
+++ mythical-creatures/exercises/hobbit.js
@@ -1,20 +1,59 @@
 function createHobbit(name, age){
   var hobbit = {
     name: name || "unknown",
-    age: age || 0, 
+    age: age || 0,
+    isAdult: false,
+    isOld: false,
+    acquaintances: [],
   }
   return hobbit
 }
 
 function celebrateBirthday(hobbit){
   hobbit.age += 1
-  return hobbit.age
+  if (hobbit.age > 32){
+    hobbit.isAdult = true
+  }
+  if (hobbit.age > 100){
+    hobbit.isOld = true
+  }
+  return hobbit
+}
+
+function getRing(hobbit){
+  if (hobbit.name === "Frodo"){
+    return 'Here is the ring!'
+  } else {
+    return 'You can\'t have it!'
+  }
+}
+
+// function meetPeople(hobbit, people){
+//   hobbit.acquaintances.push(people)
+//   return hobbit
+// }
+
+function meetPeople(hobbit, people){
+  for (var i=0; i<people.length; i++){
+    hobbit.acquaintances.push(people[i])
+  }
+  return hobbit
+}
+
+function findFriends(socialHobbit){
+  var friends = []
+  for (var i=0; i<socialHobbit.acquaintances.length; i++){
+    if (socialHobbit.acquaintances[i].relationship === "friend"){
+      friends.push(socialHobbit.acquaintances[i].name)
+    }
+  }
+  return friends
 }
 
 module.exports = {
   createHobbit, 
   celebrateBirthday, 
-  // getRing, 
-  // meetPeople, 
-  // findFriends
+  getRing, 
+  meetPeople, 
+  findFriends
 }
