ffc6535ad6d008da1c83e3fcd3939062d12a1ed7
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-04-03 17:59:37 -0600

    Complete the fav foods test suite

--- favorite-foods/favorite-foods.js
+++ favorite-foods/favorite-foods.js
@@ -1,4 +1,42 @@
+function createFavoriteFood({dish: food, ingredients, isSpicy}){
+    var dish = {
+        name: food,
+        ingredients,
+        isSpicy,
+        timesOrdered: 0
+    }
+    return dish
+}
 
+function commentOnSpiciness(dish){
+    if(dish.isSpicy){
+        return `Wow, this ${dish.name} is so spicy!`
+    } else {
+        return `Phew, this ${dish.name} is not very spicy.`
+    }
+}
 
+// function orderFood(dish){
+//     var orderedDish = {
+//         timesOrdered: 1
+//     }
+//     return orderedDish
+// }
 
-module.exports = { };
+function orderFood(dish){
+    dish.timesOrdered += 1
+    return dish
+}
+
+function createShoppingList([meal1, meal2]){
+    var list = [];
+    list = meal1.ingredients.concat(meal2.ingredients)
+    return list
+}
+
+module.exports = { 
+    createFavoriteFood,
+    commentOnSpiciness,
+    orderFood,
+    createShoppingList
+};
