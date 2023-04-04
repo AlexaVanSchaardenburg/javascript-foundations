31e43a7acd03c791c3e261cb3fd65907d1ded449
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-04-03 18:39:53 -0600

    Start the video games test suite

--- video-games/video-games.js
+++ video-games/video-games.js
@@ -1,4 +1,28 @@
+function createPlayer(name, age, moveset){
+    var player = {
+        name: name,
+        age: age,
+        moveset
+    }
+    return player
+}
 
+function createLevel(name, players){
+    var level = {
+        name: name,
+        players,
+        coins: 0,
+        lives: 3
+    }
+    return level
+}
 
+function findCoins(){
+    level.coins 
+}
 
-module.exports = { };
+module.exports = { 
+    createPlayer,
+    createLevel,
+    findCoins
+};
