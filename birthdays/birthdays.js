05519e7741d2b5919a8211666bda8a2e2b175851
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-04-05 16:01:24 -0600

    Will complete the birthdays test suite

--- birthdays/birthdays.js
+++ birthdays/birthdays.js
@@ -1,3 +1,31 @@
+function createBirthday(name, month, day){
+    birthday ={
+        name: name,
+        month: month,
+        day: day,
+    }
+    return birthday
+}
 
+function celebrateBirthday(birthday){
+    return `Today is ${birthday.month}/${birthday.day}! Happy birthday, ${birthday.name}!`
+}
 
-module.exports = {  };
+   //return number of birthdays in one month
+        //if month in the birthday element (dot notation and/or bracket notation) is equal to the month parameter then push that birthday into a new array
+
+function countBirthdays(birthdays, month){
+    var birthdaysPerMonth = []
+    for (var i=0; i<birthdays.length; i++){
+        if (birthdays[i].month === month) {
+            birthdaysPerMonth.push(birthdays[i])
+        }
+    }
+    return birthdaysPerMonth.length
+}
+
+module.exports = { 
+    createBirthday,
+    celebrateBirthday,
+    countBirthdays
+ };
