3c9861cf1608d406042a6433022860f01f73b6fb
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-04-17 14:21:01 -0600

    Start the barber test suite

--- barber-shop/barber.js
+++ barber-shop/barber.js
@@ -1,3 +1,23 @@
+function createBarber(name, earnings, knownCuts){
+    var barber = {
+        name: name,
+        earnings: earnings || 0,
+        haircuts: knownCuts || []
+    }
+    return barber
+};
 
+function giveCompliment(haircut){
+    return `This ${haircut.style} looks great!`
+};
 
-module.exports = {  };
+function cutHair(barber, haircut){
+    barber.haircuts.push(haircut)
+    return barber
+}
+
+module.exports = {
+    createBarber,
+    giveCompliment,
+    cutHair,
+ };
