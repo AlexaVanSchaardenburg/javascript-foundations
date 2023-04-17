ac022d8a53516232834c04b8881a9bc3baa5c1d1
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-04-17 14:07:39 -0600

    Complete the calendar test suite

--- calendar/calendar.js
+++ calendar/calendar.js
@@ -1,15 +1,37 @@
 function createEvent(name, month, day){
+if (day<32 && day>0){
     var event = {
         title: name,
         month: month,
         day: day,
     }
     return event
+} else {
+    return `Error: ${day} is not a valid day`
+}
 }
 
-//if day === 0 || day > 31 then return error message
+function createCalendar(name, events){
+    var calendar = {
+        owner: name,
+        events: events
+    }
+    return calendar
+}
+
+function reportMonthlyEvents(calendar, month){
+    var monthlyEvents = []
+    for (var i=0; i<calendar.events.length; i++){
+        if (calendar.events[i].month === month){
+            monthlyEvents.push(calendar.events[i])
+        }
+    }
+    return monthlyEvents
+}
 
 
 module.exports = {  
-    createEvent
+    createEvent,
+    createCalendar,
+    reportMonthlyEvents
 };
--- spotify/collection.js
+++ spotify/collection.js
@@ -1,4 +1,56 @@
+function createCollection(name, description){
+  var collection = {
+    name: name,
+    description: description,
+    tracks: []
+  }
+  return collection
+}
 
+function createTrack(title, artist, duration){
+  var track = {
+    title: title || 'unknown',
+    artist: artist || 'unknown',
+    duration: duration || 0,
+  }
+  return track
+}
 
+function reviewTrack(track){
+  if(track.artist === "Red Hot Chili Peppers") {
+    return `The song ${track.title} rules!`
+  } else {
+    return 'I wish this was a Red Hot Chili Peppers song.'
+  }
+}
 
-  module.exports = {  }
+function addTrack(collection, track){
+  collection.tracks.push(track)
+  return collection
+}
+
+function getTotalDuration(collection){
+  var totalDuration = 0
+  for (var i=0; collection.tracks.length>i; i++){
+    totalDuration += collection.tracks[i].duration
+  }
+  return totalDuration
+}
+
+function findTracksByArtist(collection, artist){
+  var songsByArtist = [];
+  for (var i=0; collection.tracks.length>i; i++){
+    if(collection.tracks[i].artist === artist){
+      songsByArtist.push(collection.tracks[i])
+    }
+  }
+}
+
+  module.exports = { 
+    createCollection,
+    createTrack,
+    reviewTrack,
+    addTrack,
+    getTotalDuration,
+    findTracksByArtist
+   }
